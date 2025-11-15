ce65b2daf594961766e1775257a390d1a2bd12d0
Matthew Franchini <matchewf11@gmail.com>
2025-11-14 19:23:31 -0800

    foo

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,10 +4,10 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    // insert a db into the thing
+
     let app = Router::new().route("/hello-world", get(hello_world));
-
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-
     let listener = TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app.into_make_service())
         .await
